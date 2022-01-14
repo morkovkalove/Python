0257b5337e6e654b077f42fd5b48a9bbe34c5b45
morkovkalove <73894426+morkovkalove@users.noreply.github.com>
2022-01-14 18:05:32 +0300

    Update My_OOP2.py

--- My_OOP2.py
+++ My_OOP2.py
@@ -2,7 +2,7 @@ class Barbershop:
 	__name = None  #Инкапсуляция
 	__subway = None
 
-	def __init__(self, name, subway):
+	def __init__(self, name, subway): #Конструктор
 		self.name = name
 		self.subway = subway
 
@@ -12,13 +12,13 @@ class Barbershop:
 class Barbers(Barbershop):   #Наследование
 	staff = 0
 
-	def __init__(self, staff, name, subway):   #Полиморфизм
-		super(Barbers, self).__init__(name, subway)
+	def __init__(self, staff, name, subway):   
+		super(Barbers, self).__init__(name, subway) #Полиморфизм
 		self.staff = staff
 
 	def print_info(self):
 		super().print_info()
 		print("Sraff:", self.staff, "people")
 
-barbers = Barbers(15, "Brutal", "Voykovskaya")
-barbers.print_info()
+barbers = Barbers(15, "Brutal", "Voykovskaya") #Передача параметров: 15 барберов, Название Барбершопа "Brutal", Месторасположение : метро "Voykovskaya".
+barbers.print_info() #Вывод информации
